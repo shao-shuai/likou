856a4eebd10d65649d6f774a9698508bcb0571cb
Shuai Shao <shuai-shao@outlook.com>
2022-12-13 17:19:48 -0800

    add deque to sum bt's values

--- count_binary_tree_nodes.py
+++ count_binary_tree_nodes.py
@@ -1,2 +1,51 @@
 # We can use both DFS and BFS to count the nodes of binary tree
 # https://leetcode.com/problems/count-complete-tree-nodes/description/
+
+from collections import deque
+
+def dqueue_count(root):
+    if root is None:
+        return 0
+
+    q = deque()
+    q.append(root)
+    ans = 0
+    
+    while len(q) > 0:
+        p = q.popleft()
+        ans += p.val
+        if p.left:
+            q.append(p.left)
+        if p.right:
+            q.append(p.right)
+
+    return ans
+
+class Node:
+
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def addLeft(self, val):
+        newNode = Node(val)
+        self.left = newNode
+
+        return newNode
+
+    def addRight(self, val):
+        newNode = Node(val)
+        self.right = newNode
+
+        return newNode
+
+root = Node(1)
+root.addLeft(2)
+right = root.addRight(3)
+right.addLeft(4)
+right.addRight(5)
+
+print(dqueue_count(root))
+
+    
