ef6875b85d83929c114cf669188d83a17704b5cd
shao-shuai <shuai-shao@outlook.com>
2022-12-23 14:06:50 -0800

    add two sum

---
+++ two_sum.py
@@ -0,0 +1,33 @@
+# Brute force
+def find_two_b(nums, target):
+    for i in range(len(nums)):
+        for j in range(i):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+# Dictionary
+def find_two(nums, target):
+    dict = {}
+    for i in range(len(nums)):
+        if (target - nums[i]) not in dict:
+            dict[nums[i]] = i
+        else:
+            return [i, dict[target - nums[i]]]
+
+# Two pointers
+def find_two_p(nums, target):
+    nums.sort()
+
+    small = 0
+    large = len(nums) - 1
+
+    while small < large:
+        if nums[small] + nums[large] < target:
+            small += 1
+        elif nums[small] + nums[large] > target:
+            large -= 1
+        else:
+            return true
+
+a = [3,2,4]
+print(find_two_b(a, 6))
